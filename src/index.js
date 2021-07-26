bdd87b712c61ae13d28a23603652f1f3dce61e2e
David Basabe <david.basabe@cencosud.cl>
2021-07-26 19:53:57 -0400

    :rocket: Hash router

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { FavoritesContextProvider } from "./store/favorites-context";
 
 ReactDOM.render(
   <ChakraProvider>
     <FavoritesContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </FavoritesContextProvider>
   </ChakraProvider>,
   document.getElementById("root")
