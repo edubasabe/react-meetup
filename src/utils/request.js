7464d476e08b67fb09a74340bcebefdf2dcfc1f7
David Basabe <david.basabe@cencosud.cl>
2021-07-26 19:38:42 -0400

    :recycle: Meetups for local

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,35 @@
 export function getMeetups() {
+  /* When using firebase
   return fetch(
     "https://react-meetup-fe77a-default-rtdb.firebaseio.com/meetups.json"
   ).then((res) => res.json());
+  */
+  const meetups = localStorage.getItem("meetups");
+  if (!meetups) {    
+    const initialMeetups = [
+      {
+        id: "a1",
+        image: "https://source.unsplash.com/wn7dOzUh3Rs/320x280",
+        title: "Initial Meetup",
+        description: "This is the first meetup for the app"
+      }
+    ];
+    localStorage.setItem("meetups", JSON.stringify(initialMeetups));
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(initialMeetups);
+      }, 2000);
+    }); 
+  }
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(JSON.parse(meetups));
+    }, 2000);
+  }); 
 }
 
 export function postMeetup(meetup) {
+/* When using firebase  
   return fetch(
     "https://react-meetup-fe77a-default-rtdb.firebaseio.com/meetups.json",
     {
@@ -14,5 +39,13 @@ export function postMeetup(meetup) {
         "Content-Type": "application/json",
       },
     }
-  );
+  ); */
+  const meetups = JSON.parse(localStorage.getItem("meetups"));
+  meetups.push(meetup);
+  localStorage.setItem("meetups", JSON.stringify(meetups));
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(meetups);
+    }, 3000)
+  });
 }
